202de0a332ebdefe49dbc6c88d056fb3ef077814
Luiz do Valle <lld2131@columbia.edu>
2023-07-27 23:22:37 -0400

    Complete Chapter 9 - Functions

--- printf01.s
+++ printf01.s
@@ -1,10 +1,10 @@
 .data
 
 .balign 4
-message1: .asciz "Hey, type a number: "
+message1: .asciz "Type a number: "
 
 .balign 4
-message2: .asciz "I read the number %d\n"
+message2: .asciz "%d times 5 is %d\n"
 
 .balign 4
 scan_pattern: .asciz "%d"
@@ -19,8 +19,8 @@ return: .word 0
 
 .global main
 main:
-    ldr r1, addr_of_return
-    str lr, [r1]
+    ldr r0, addr_of_return
+    str lr, [r0]
 
     ldr r0, addr_of_message1
     bl printf
@@ -29,6 +29,11 @@ main:
     ldr r1, addr_of_number
     bl scanf
 
+    ldr r0, addr_of_number
+    ldr r0, [r0]
+    bl mult_5
+    
+    mov r2, r0
     ldr r0, addr_of_message2
     ldr r1, addr_of_number
     ldr r1, [r1]
@@ -42,11 +47,15 @@ main:
 
     bx lr
 
-addr_of_return: .word return
+mult_5:
+    add r0, r0, LSL #2
+    bx lr
+
 addr_of_message1: .word message1
+addr_of_message2: .word message2
 addr_of_scan_pattern: .word scan_pattern
 addr_of_number: .word number
-addr_of_message2: .word message2
+addr_of_return: .word return
 
 .global printf
 .global scanf
