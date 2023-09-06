1d7e186cb86b9f2880768ef173814fa367f5e648
Luiz do Valle <lld2131@columbia.edu>
2023-09-05 23:41:28 -0400

    Add macro and constant files for chapter 7

---
+++ raspberrypi_assembly_language_programming_book/chapter7/fileio.s
@@ -0,0 +1,40 @@
+// Macros to perform file I/O
+
+.include "unistd.s"
+
+.equ O_RDONLY, 0
+.equ O_WRONLY, 1
+.equ O_CREAT, 0100
+.equ S_RDWR, 0666
+
+.macro openFile fileName, flags
+    ldr r0, =\fileName
+    mov r1, #\flags
+    mov r2, #S_RDWR // RW access
+    mov r7, #sys_open
+    svc 0
+.endm
+.macro readFile fd, buffer, length
+    mov r0, \fd
+    mov r1, =\buffer
+    mov r2, #\length
+    mov r7, #sys_read
+    svc 0
+.endm
+.macro writeFile fd, buffer, length
+    mov r0, \fd
+    mov r1, =\buffer
+    mov r2, #\length
+    mov r7, #sys_write
+    svc 0
+.end
+.macro flushClose fd
+// fsync call
+    mov r0, \fd
+    mov r7, #sys_fsync
+    svc 0
+// close call
+    mov r0, \fd
+    mov r7, #sys_close
+    svc 0
+.endm
