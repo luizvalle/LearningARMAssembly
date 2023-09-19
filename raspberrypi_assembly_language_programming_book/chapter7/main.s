47f7db70a30104f9019afcc3aa045f1351abbc76
Luiz do Valle <lld2131@columbia.edu>
2023-09-18 23:56:42 -0400

    Chapter 7: Added basic error handling

--- raspberrypi_assembly_language_programming_book/chapter7/main.s
+++ raspberrypi_assembly_language_programming_book/chapter7/main.s
@@ -6,11 +6,28 @@
 _start:
     // Open input file
     openFile in_file, O_RDONLY
-    mov r8, r0 // Save file descriptor
+    movs r8, r0 // Save file descriptor and set flags
+    bpl success // Function did not return an error code (non-negative)
 
+    // If we reach this, then an error was returned
+    mov r0, #1 // stdout
+    ldr r2, =in_err_size
+    ldr r2, [r2]
+    writeFile r0, in_err, r2
+    flushClose r8
+    b exit
+
+success:
     // Open output file
     openFile out_file, O_CREAT+O_WRONLY
     mov r9, r0 // Save file descriptor
+    bpl loop
+
+    mov r0, #1 // stdout
+    ldr r2, =out_err_size
+    ldr r2, [r2]
+    writeFile r0, out_err, r2
+    b exit
 
 loop:
     // Read input file
@@ -37,6 +54,7 @@ end_loop:
     flushClose r8
     flushClose r9
 
+exit:
     // Exit
     mov r7, #1
     mov r0, #0
@@ -48,3 +66,8 @@ in_file: .asciz "main.s"
 out_file: .asciz "output.txt"
 in_buffer: .fill BUFFER_SIZE + 1, 1, 0
 out_buffer: .fill BUFFER_SIZE + 1, 1, 0
+in_err: .asciz "Error opening the input file.\n"
+// . is a special GNU variable representing the current address the assembler is on
+in_err_size: .word .-in_err 
+out_err: .asciz "Error opening the ouput file.\n"
+out_err_size: .word .-out_err
