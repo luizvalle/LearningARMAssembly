d48f8169d6541d7881290772a8d95863c8ac78cd
Luiz do Valle <lf.lemos.valle@gmail.com>
2023-12-24 17:43:08 -0500

    Implement a helper vector dot product function

--- raspberrypi_assembly_language_programming_book/chapter10/multiply_and_accumulate/main.s
+++ raspberrypi_assembly_language_programming_book/chapter10/multiply_and_accumulate/main.s
@@ -1,26 +1,31 @@
 .include "fileio.s"
 
+.equ N, 3 // Number of rows and columns in the matrices
+
 .text
 .global main
 main:
   push {r4-r12, lr}
   print_str "Matrix A:\n"
   ldr r0, =A
-  mov r1, #3
+  mov r1, #N
   bl print_matrix
   print_str "Matrix B:\n"
   ldr r0, =B
-  mov r1, #3
+  mov r1, #N
   bl print_matrix
   print_str "Matrix AB = C:\n"
+// Calulate C := AB
   ldr r0, =A
   ldr r1, =B
   ldr r2, =C
-  mov r3, #3
+  mov r3, #N
   bl matrix_multiply
+// Print C
   ldr r0, =C
-  mov r1, #3
+  mov r1, #N
   bl print_matrix
+// Cleanup and exit
   pop {r4-r12, lr}
   mov r0, #0
   bx lr
--- raspberrypi_assembly_language_programming_book/chapter10/multiply_and_accumulate/matrix_multiply.s
+++ raspberrypi_assembly_language_programming_book/chapter10/multiply_and_accumulate/matrix_multiply.s
@@ -7,6 +7,31 @@
 //  r3: Number of rows in each matrix
 .global matrix_multiply
 matrix_multiply:
-  push {r4-r12, lr}
-  pop {r4-r12, lr}
-  bx lr
+    push {r4-r12, lr}
+    pop {r4-r12, lr}
+    bx lr
+
+// Calculates the dot product of two vectors
+// Parameters
+//  r0: Address pointing to the first element of vector x
+//  r1: Address pointing to the first element of vector y
+//  r2: Length of both vectors
+// Returns:
+//  r0: The upper 32 bits of a * b
+//  r1: The lower 32 bits of a * b
+dot_product:
+    push {r4-r12, lr}
+    mov r4, #0 // Low order accumulator
+    mov r5, #0 // High order accumulator
+loop:
+    subs r2, r2, #1
+    blt exit_dp // If r2 is negative
+    ldr r8, [r0], #4 // x[i]
+    ldr r9, [r1], #4 // y[i]
+    smlal r4, r5, r8, r9 
+    b loop
+exit_dp:
+    mov r0, r5
+    mov r1, r4
+    pop {r4-r12, lr}
+    bx lr
